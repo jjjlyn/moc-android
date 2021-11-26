7656e4106cb281b36923aec667c814f3890c9420
jjjlyn <prize1142@gmail.com>
2021-11-26 19:00:29 +0900

    #9 chore: 린트 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,62 @@
-allprojects {
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    kotlin("android") apply false
+    id("io.gitlab.arturbosch.detekt") version BuildPluginsVersion.deteKt
+    id("org.jlleitschuh.gradle.ktlint") version BuildPluginsVersion.ktLint
+}
+
+buildscript {
     repositories {
         google()
         mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${BuildPluginsVersion.gradle}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${BuildPluginsVersion.kotlin}")
+        classpath("com.google.gms:google-services:${BuildPluginsVersion.gms}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${BuildPluginsVersion.safeArgs}")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:${BuildPluginsVersion.crashlytics}")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:${BuildPluginsVersion.hilt}")
+    }
+}
+
+allprojects {
+    group = publishingGroup
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("io.gitlab.arturbosch.detekt")
+        plugin("org.jlleitschuh.gradle.ktlint")
+    }
+
+    ktlint {
+        debug.set(false)
+        version.set(Version.ktLint)
+        verbose.set(true)
+        android.set(false)
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+        enableExperimentalRules.set(true)
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+    }
+
+    detekt {
+        config = rootProject.files("config/detekt/detekt.yml")
+        reports {
+            html {
+                enabled = true
+                destination = file("build/reports/detekt.html")
+            }
+        }
     }
 }
 
