9e7d3dd3e344cbd81fdc25c3c4f3c51ef1864b58
jjjlyn <prize1142@gmail.com>
2021-12-31 01:40:44 +0900

    chore(model): 디펜던시 추가, 프로가드 설정

---
+++ model/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = Sdk.compile
+
+    defaultConfig {
+        minSdk = Sdk.min
+        targetSdk = Sdk.target
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(Library.kotlinLib)
+    implementation(Library.appcompat)
+    implementation(Library.core)
+    testImplementation(Library.junit)
+    androidTestImplementation(Library.testExtJunit)
+    androidTestImplementation(Library.espressoCore)
+}
