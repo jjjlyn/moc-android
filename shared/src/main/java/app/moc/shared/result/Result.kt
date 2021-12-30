04cdcfeecfb3e08ed35803ff37eca66acd098fdc
jjjlyn <prize1142@gmail.com>
2021-12-31 01:48:57 +0900

    feat(shared): 공통 wrapper 구현

---
+++ shared/src/main/java/app/moc/shared/result/Result.kt
@@ -0,0 +1,26 @@
+package app.moc.shared.result
+
+import app.moc.shared.result.Result.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
+sealed class Result<out R> {
+
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val throwable: Throwable) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+
+    val isLoading get() = this is Loading
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data]"
+            is Error -> "Error[exception=$throwable]"
+            Loading -> "Loading"
+        }
+    }
+}
+
+val <T> Result<T>.data: T?
+    get() = (this as? Success)?.data
