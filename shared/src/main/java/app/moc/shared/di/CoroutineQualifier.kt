bc7785f548745297dd3a05329e034a67d49a96e3
jjjlyn <prize1142@gmail.com>
2022-01-02 15:55:15 +0900

    feat(shared - di): 코루틴 qualifier 추가

--- shared/src/main/java/app/moc/shared/di/CoroutineQualifier.kt
+++ shared/src/main/java/app/moc/shared/di/CoroutineQualifier.kt
@@ -4,4 +4,12 @@ import javax.inject.Qualifier
 
 @Retention(AnnotationRetention.BINARY)
 @Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
 annotation class IODispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class ApplicationScope
