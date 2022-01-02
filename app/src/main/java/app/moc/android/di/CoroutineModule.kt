faa97357e3cc36cebc856e7d18df6b96281e9181
jjjlyn <prize1142@gmail.com>
2022-01-02 15:36:40 +0900

    feat(app - di): coroutine default dispatcher 추가

--- app/src/main/java/app/moc/android/di/CoroutineModule.kt
+++ app/src/main/java/app/moc/android/di/CoroutineModule.kt
@@ -1,16 +1,25 @@
 package app.moc.android.di
 
+import app.moc.shared.di.DefaultDispatcher
 import app.moc.shared.di.IODispatcher
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object CoroutineModule {
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
 
     @IODispatcher
     @Provides
