98142308cac09d99fef9a2f561d3a9439bd51d80
jjjlyn <prize1142@gmail.com>
2022-01-02 15:29:10 +0900

    feat(app - di): application scope 추가

--- app/src/main/java/app/moc/android/di/AppModule.kt
+++ app/src/main/java/app/moc/android/di/AppModule.kt
@@ -6,33 +6,26 @@ import app.moc.shared.data.auth.AuthRepository
 import app.moc.shared.data.auth.DefaultAuthRepository
 import app.moc.shared.data.business.BusinessRepository
 import app.moc.shared.data.business.DefaultBusinessRepository
+import app.moc.shared.di.ApplicationScope
+import app.moc.shared.di.DefaultDispatcher
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
 
-    @Provides
+    @ApplicationScope
     @Singleton
-    fun authRepository(
-            service: AuthService
-    ): AuthRepository {
-        return DefaultAuthRepository(
-                service
-        )
-    }
+    @Provides
+    fun providesApplicationScope(
+        @DefaultDispatcher defaultDispatcher: CoroutineDispatcher
+    ): CoroutineScope = CoroutineScope(SupervisorJob() + defaultDispatcher)
 
-    @Provides
-    @Singleton
-    fun businessRepository(
-            service: BusinessService
-    ): BusinessRepository {
-        return DefaultBusinessRepository(
-                service
-        )
-    }
 }
